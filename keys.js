5a5a7ff7add81110567d06ac9971c635a4b8958a
arifweb2020 <arifhussainb2019@gmail.com>
2021-10-10 16:12:38 +0530

    final app

--- keys.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://arif21:265DFTLC6W0wytXP@cluster0.3lae7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:"arifarifffhgfhfhgfthtfd"
-}
